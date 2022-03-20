9d0b7ac2ff49015a3cb5003f644e41dccb55b6eb
Timur4ick <79656475+Timur4ick@users.noreply.github.com>
2022-03-20 19:49:10 +0200

    modyle 1.1.

--- .vscode/src/index.js
+++ .vscode/src/index.js
@@ -201,16 +201,16 @@ console.log("HELLO");
 //   .map((user) => user.name);
 // console.log(getNamesSortedByFriendCount);
 
-const orders = [
-  { email: "solomon@topmail.ua", dish: "Burger" },
-  { email: "artemis@coldmail.net", dish: "Pizza" },
-  { email: "jacob@mail.com", dish: "Taco" },
-];
+// const orders = [
+//   { email: "solomon@topmail.ua", dish: "Burger" },
+//   { email: "artemis@coldmail.net", dish: "Pizza" },
+//   { email: "jacob@mail.com", dish: "Taco" },
+// ];
 
 // Пиши код ниже этой строки
-function composeMessage(position) {
-  return `'Готовим ${this.dish} для ${this.email} Ваш заказ ${
-    position + 1
-  }-й в очереди.'`;
-}
-const messages = [];
+// function composeMessage(position) {
+//   return `'Готовим ${this.dish} для ${this.email} Ваш заказ ${
+//     position + 1
+//   }-й в очереди.'`;
+// }
+// const messages = [];
